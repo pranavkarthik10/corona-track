c3c04fcde3968a0f3cab0cc4c2f7618178225592
pranavkarthik10 <pranav.karthik@icloud.com>
2020-03-13 20:14:43 -0700

    Initial Commit

---
+++ CoronaTrack/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CoronaTrack
+//
+//  Created by Pranav Karthik on 2020-03-13.
+//  Copyright © 2020 Pranav Karthik. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
